8fc5d7805f7eeaa8aa4f7e2daca2f812dfe81ec0
Ryan Christensen <a023107782@usu.edu>
2024-07-16 17:08:09 -0600

    run simulation

--- src/main/kotlin/TrackingSimulator.kt
+++ src/main/kotlin/TrackingSimulator.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 class TrackingSimulator {
     private var shipments = mutableListOf<Shipment>()
 
@@ -9,6 +11,22 @@ class TrackingSimulator {
         }
         return null
     }
-    
-//    fun addShipment(shipment: Shipment)
+
+    fun addShipment(shipment: Shipment) {
+        shipments.add(shipment)
+    }
+
+    suspend fun runSimulation() {
+        val allLines = mutableListOf<String>()
+
+        File ("./src/main/resources/test.txt").reader().useLines{
+            lines ->lines.forEach {
+                allLines.add (it)
+            }
+        }
+
+        allLines.forEach {
+            val shipmentTake = it.split(",")
+        }
+    }
 }
