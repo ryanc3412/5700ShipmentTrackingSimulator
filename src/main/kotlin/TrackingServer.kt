8e5cf5d22b6b911dae85c1e9538fd628ed4d519c
Ryan Christensen <a023107782@usu.edu>
2024-07-23 15:10:53 -0600

    First implementation of tracking server implementation

--- src/main/kotlin/TrackingServer.kt
+++ src/main/kotlin/TrackingServer.kt
@@ -1,4 +1,10 @@
 package com.example
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import kotlinx.coroutines.delay
 import java.io.File
 
@@ -8,48 +14,91 @@ class TrackingServer {
         private var shipments = mutableMapOf<String, Shipment>()
 
         suspend fun runServer() {
-            embeddedServer(Netty, 3000) {
+            embeddedServer(Netty, port = 8000) {
+                routing {
+                    get("/") {
+                        val file = File("webpage.html")
+                        val contents = file.readText()
+                        call.respondText(contents, ContentType.Text.Html)
 
-            }
+                        var pl = ""
+
+                        if (call.parameters["say"] != null) {
+                            call.parameters["say"]?.let { it1 -> pl = it1 }
+
+                            println("> " + pl)
+                            val item = pl.split(",")
+                            when (item[0]) {
+                                "created" -> {
+                                    addShipment(ShippingUpdateCreated(item[1], item[2].toLong(), item[3]).toShipment)
+                                }
+                                "shipped" -> {
+                                    findShipment(item[1])?.let { it1 ->
+                                        ShippingUpdateShipped(
+                                            it1,
+                                            item[2].toLong(),
+                                            item[3].toLong()
+                                        )
+                                    }
+                                }
+                                "location" -> {
+                                    findShipment(item[1])?.let { it1 ->
+                                        ShippingUpdateLocation(
+                                            it1,
+                                            item[2].toLong(),
+                                            item[3]
+                                        )
+                                    }
+                                }
+                                "delivered" -> {
+                                    findShipment(item[1])?.let { it1 -> ShippingUpdateDelivered(it1, item[2].toLong()) }
+                                }
+                                "delayed" -> {
+                                    findShipment(item[1])?.let { it1 ->
+                                        ShippingUpdateDelayed(
+                                            it1,
+                                            item[2].toLong(),
+                                            item[3].toLong()
+                                        )
+                                    }
+                                }
+                                "lost" -> {
+                                    findShipment(item[1])?.let { it1 -> ShippingUpdateLost(it1, item[2].toLong()) }
+                                }
+                                "canceled" -> {
+                                    findShipment(item[1])?.let { it1 -> ShippingUpdateCanceled(it1, item[2].toLong()) }
+                                }
+                                "noteadded" -> {
+                                    findShipment(item[1])?.let { it1 ->
+                                        ShippingUpdateNoteAdded(
+                                            it1,
+                                            item[2].toLong(),
+                                            item[3]
+                                        )
+                                    }
+                                }
+                                else -> {
+                                    println("Strategy not found")
+                                }
+                            }
+                            if (findShipment(item[1]) == null) {
+                                println("> Added")
+                            } else {
+                                println("> Found")
+                            }
+                        }
+                    }
+                }
+            }.start(wait = true)
+            delay(1000)
         }
 
-//        fun findShipment(id: String) : Shipment? {
-//            shipments.forEach {
-//                if(it.id == id) {
-//                    return it
-//                }
-//            }
-//            return null
-//        }
-//
-//        fun addShipment(shipment: Shipment) {
-//            shipments.add(shipment)
-//        }
-//
-//        private fun checkStrategy(shipmentPiece: List<String>) {
-//            when (shipmentPiece[0]) {
-//                "created" -> {addShipment(ShippingUpdateCreated(shipmentPiece[1], shipmentPiece[2].toLong()).toShipment)}
-//                "shipped" -> {findShipment(shipmentPiece[1])?.let { it1 -> ShippingUpdateShipped(it1, shipmentPiece[2].toLong(), shipmentPiece[3].toLong())}}
-//                "location" -> {findShipment(shipmentPiece[1])?.let { it1 -> ShippingUpdateLocation(it1, shipmentPiece[2].toLong(), shipmentPiece[3])}}
-//                "delivered" -> {findShipment(shipmentPiece[1])?.let { it1 -> ShippingUpdateDelivered(it1, shipmentPiece[2].toLong())}}
-//                "delayed" -> {findShipment(shipmentPiece[1])?.let { it1 -> ShippingUpdateDelayed(it1, shipmentPiece[2].toLong(), shipmentPiece[3].toLong())}}
-//                "lost" -> {findShipment(shipmentPiece[1])?.let { it1 -> ShippingUpdateLost(it1, shipmentPiece[2].toLong())}}
-//                "canceled" -> {findShipment(shipmentPiece[1])?.let { it1 -> ShippingUpdateCanceled(it1, shipmentPiece[2].toLong())}}
-//                "noteadded" -> {findShipment(shipmentPiece[1])?.let { it1 -> ShippingUpdateNoteAdded(it1, shipmentPiece[2].toLong(), shipmentPiece[3])}}
-//                else -> {
-//                    println("Unknown strategy entered")
-//                }
-//            }
-//        }
-//
-//        suspend fun runSimulation() {
-//            val filePath = "./src/main/resources/test.txt"
-//            val allLines = File(filePath).readLines()
-//            allLines.forEach { line ->
-//                val shipmentPiece = line.split(",")
-//                checkStrategy(shipmentPiece)
-//                delay(1000)
-//            }
-//        }
+        fun findShipment(id: String) : Shipment? {
+            return shipments.get(id)
+        }
+
+        fun addShipment(shipment: Shipment) {
+            shipments.put(shipment.id, shipment)
+        }
     }
 }
