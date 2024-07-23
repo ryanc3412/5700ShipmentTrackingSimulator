2c11403788dbc60e8b9bf02423cd064621120afe
Ryan Christensen <a023107782@usu.edu>
2024-07-23 13:30:05 -0600

    Express Shipment

--- src/main/kotlin/ExpressShipment.kt
+++ src/main/kotlin/ExpressShipment.kt
@@ -1,2 +1,12 @@
-class ExpressShipment {
+import com.example.Shipment
+
+class ExpressShipment(status: String,
+                      id: String,
+                      created: Long
+) : Shipment(status, id, created, type = "express") {
+
+    override fun isValid(): Boolean {
+        val threeDaysInMillis: Long = 3 * 24 * 60 * 60 * 1000L
+        return (dateTime - created) < threeDaysInMillis
+    }
 }
