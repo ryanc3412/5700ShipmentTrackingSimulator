8231842e6de4225982ae6456acdb8dd46692c590
Ryan Christensen <a023107782@usu.edu>
2024-07-23 14:23:28 -0600

    Bulk shipment

--- src/main/kotlin/BulkShipment.kt
+++ src/main/kotlin/BulkShipment.kt
@@ -5,6 +5,7 @@ class BulkShipment(
     id: String,
     created: Long
 ) : Shipment(status, id, created, type = "bulk") {
+
     override fun isValid(): Boolean {
         return (dateTime - created) >= 259200000L
     }
